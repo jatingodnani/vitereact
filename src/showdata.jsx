b98fa23accaceba7e82214188c7c8620cba1a5a5
KartikJoshiUK <kartikjoshiuk@gmail.com>
2024-06-20 14:00:55 +0530

    fixed suspense for data fetching

--- src/showdata.jsx
+++ src/showdata.jsx
@@ -1,33 +1,28 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import createResource from "./suspender";
 
 export const fetchData = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([
-          { name: "John Doe", email: "john@example.com" },
-          { name: "Jane Doe", email: "jane@example.com" },
-        ]);
-      }, 5000);
-    });
-  };
-  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { name: "John Doe", email: "john@example.com" },
+        { name: "Jane Doe", email: "jane@example.com" },
+      ]);
+    }, 5000);
+  });
+};
 
-
+const resource = createResource(fetchData());
 
 const Showdata = () => {
-  const [jsdata, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData().then((data) => {
-      setData(data);
-    });
-  }, []);
-
+  const [jsdata, setJsdata] = useState(resource.read());
   return (
     <div>
       {jsdata.map((each) => (
-        <div style={{ background: "red", border: "2px solid black" }} key={each.email}>
+        <div
+          style={{ background: "red", border: "2px solid black" }}
+          key={each.email}
+        >
           <p>{each.name}</p>
           <p>{each.email}</p>
         </div>
